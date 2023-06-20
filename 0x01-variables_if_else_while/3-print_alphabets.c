9b814558a956e1d6adcfa7fb14d9f985b40c0c64
Johnson Adeoye <johnsonadeoye888@gmail.com>
2023-06-20 15:45:21 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Print the alphabet using putchar function
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alpha[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int i;
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alpha[i]);
+	}
+	putchar('\n');
+	return (0);
+}
