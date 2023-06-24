f81aedfd3c637e6d7bd36e64d7f59e709cd9e978
Johnson Adeoye <johnsonadeoye888@gmail.com>
2023-06-24 14:38:27 -0700

    0-putchar.c

--- 0x01-variables_if_else_while/0x02-functions_nested_loops/0-putchar.c
+++ 0x01-variables_if_else_while/0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
 #include "main.h"
-#include <unistd.h>
 /**
-*_putchar - writes the character c to stdout
-*c: The character to print
+*main - print _putchar
 *
-*Return: Always 1(Success)
-*On error, -1 is returned, and errno is set appropriately.
+*Return: 0
 */
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
