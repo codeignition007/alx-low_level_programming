971eb6e7111fb293a5f4ca2e41bd65b68310c548
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-10-19 23:07:42 -0700

    This is a dynamic libraries project

--- 0x18-dynamic_libraries/6-abs.c
+++ 0x18-dynamic_libraries/6-abs.c
@@ -1,24 +1,15 @@
 #include "main.h"
 
 /**
- * _abs - computes the absolute value of an integer
- * @n: n is an integer
- *
- * Return: integer
- */
-
+* _abs - computes the absolute value of an integer
+* @n: the int to check
+* Return: the absolute value of int
+*/
 int _abs(int n)
 {
-	if (n > 0)
+	if (n >= 0)
 	{
-	return (n);
+		return (n);
 	}
-	else if (n < 0)
-	{
 	return (-n);
-	}
-	else
-	{
-	return (n);
-	}
 }
