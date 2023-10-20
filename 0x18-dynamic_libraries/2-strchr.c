6a6826b2e164085f16d91cf96f2f3b0fbf28a891
JOHNSON ADEOYE <johnsonadeoye888@gmail.com>
2023-10-19 23:47:48 -0700

    This is a dynamic libraries project

--- 0x18-dynamic_libraries/2-strchr.c
+++ 0x18-dynamic_libraries/2-strchr.c
@@ -1,18 +1,29 @@
 #include "main.h"
+
 /**
- * _strchr - Entry point
- * @s: input
- * @c: input
- * Return: Always 0 (Success)
- */
+  * _strchr - locate character in string
+  * @s: source string
+  * @c: character to find
+  *
+  * Return: the string from character found
+  */
 char *_strchr(char *s, char c)
 {
-	int i = 0;
+	int a = 0, b;
 
-	for (; s[i] >= '\0'; i++)
+	while (s[a])
 	{
-		if (s[i] == c)
-			return (&s[i]);
+		a++;
 	}
-	return (0);
+
+	for (b = 0; b <= a; b++)
+	{
+		if (c == s[b])
+		{
+			s += b;
+			return (s);
+		}
+	}
+
+	return ('\0');
 }
